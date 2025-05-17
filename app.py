f124728eff2e129ac7967b14f662c773c5368fd3
EnigmaNehA <142612711+EnigmaNehA@users.noreply.github.com>
2025-05-17 23:27:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -299,4 +299,4 @@ def predict():
 
 if __name__ == "__main__":
     debug_mode = os.getenv("FLASK_DEBUG", "false").lower() == "true"
-    app.run(host="0.0.0.0", port=5000, debug=debug_mode)
+    app.run(host="0.0.0.0", port=port , debug=debug_mode)
