c43855a2f97489e6eb4a94ba7695d7300b801d6d
EnigmaNehA <142612711+EnigmaNehA@users.noreply.github.com>
2025-05-17 11:23:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -265,4 +265,6 @@ def predict():
 
 # Entry point
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
